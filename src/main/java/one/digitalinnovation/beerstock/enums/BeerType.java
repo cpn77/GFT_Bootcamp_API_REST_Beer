f0adfd8e852bc7e38f5225ad43623ca0ed8a49f3
cpn77 <cpn77br@gmail.com>
2021-07-09 15:24:01 -0300

    Desenvolvimento de testes unitários para validar uma API REST de gerenciamento estoques de cerveja. Adicionado cerveja sem álcool no ENUM da entidade BEER. GFT Bootcamp DIO.

---
+++ src/main/java/one/digitalinnovation/beerstock/enums/BeerType.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.beerstock.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BeerType {
+
+    LAGER("Lager"),
+    MALZBIER("Malzbier"),
+    WITBIER("Witbier"),
+    WEISS("Weiss"),
+    ALE("Ale"),
+    IPA("IPA"),
+    STOUT("Stout"),
+    LIGHT("Light (Non-alcoholic)");
+
+    private final String description;
+}
---
+++ src/main/java/one/digitalinnovation/beerstock/exception/BeerStockZeroException.java
@@ -0,0 +1,13 @@
+//CPN Code
+package one.digitalinnovation.beerstock.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BeerStockZeroException extends Exception {
+
+    public BeerStockZeroException(Long id, int quantityToDecrement) {
+        super(String.format("Beers with %s ID to decrement informed exceeds the min stock capacity: %s", id, quantityToDecrement));
+    }
+}
